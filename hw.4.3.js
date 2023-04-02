0c3bc4fb07c30da358cbe8423ec694bd141c7430
Natalya Vakiryak <vakiryak11@gmail.com>
2023-04-02 17:31:50 -0400

    Changes to be committed: modified:   hw.4.3.js

--- hw.4.3.js
+++ hw.4.3.js
@@ -15,20 +15,19 @@ const employee2 = {
 compareEmployees(employee1, employee2);
 function compareEmployees(employee1, employee2) {
     if (employee1.companiesList.length < employee2.companiesList.length) {
-        console.log(employee2.name + " " + employee2.lastname);
+        console.log(employee2.name + " " + employee2.lastname + " has more companies. Here is the list of his companies:");
+        for (let index = 0; index < employee2.companiesList.length; index++) {
+            console.log(employee2.companiesList[index]);
+        }
     }
     else if (employee1.companiesList.length > employee2.companiesList.length) {
-        console.log(employee1.name + " " + employee1.lastname);
+        console.log(employee1.name + " " + employee1.lastname + " has more companies. Here is the list of his companies:");
+        for (let index = 0; index < employee1.companiesList.length; index++) {
+            console.log(employee1.companiesList[index]);
+        }
     }
     else
-        console.log(employee1.name + " " + employee1.lastname, employee2.name + " " + employee2.lastname);
-}
+        console.log(employee1.name + " and " + employee1.lastname, employee2.name + " " + employee2.lastname +
+         " have the same number of companies");
 
-
-for (let index = 0; index < employee1.companiesList.length; index++) {
-    console.log(employee1.companiesList[index]);
-
-}
-for (let index = 0; index < employee2.companiesList.length; index++) {
-    console.log(employee2.companiesList[index]);
 }
